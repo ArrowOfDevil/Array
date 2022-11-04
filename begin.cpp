983aeec6cb52fcfd15b796763c50fa8d09fc6326
ArrowOfDevil <47675041+ArrowOfDevil@users.noreply.github.com>
2022-11-04 13:19:31 +0300

    Update begin.cpp

--- begin.cpp
+++ begin.cpp
@@ -4,14 +4,18 @@ void Print1(int *arr, int Size)  {
     printf("%3d", arr[i]);
   printf("\n");
 }
+//в файле task4 вызов функции будет таким:
+//Print1(na, N); na -- название массива N -- размер
+
+//________________________________________________//
 
 void Input1(int arr[], int Size)  {
 	for (int i = 0; i < Size; i++)
 	{
 		printf("Enter %d element: ", i);
 		scanf_s("%d", &arr[i]);
-		//scanf_s("%d", arr+i); 
-		//scanf_s("%d", &i[arr]);
+		//scanf_s("%d", arr+i); для шарящих будет файл с сайта (ибо у меня он только с впн открылся)
+		//scanf_s("%d", &i[arr]); на это тоже можете особо не смотреть наш прогер выпендривался
 	}
 	getchar();
 }
